01441f3548535aa3e2e0c858938a2e539971805d
Mitchell <mitchell.ramos1028@gmail.com>
2024-06-10 23:16:30 -0400

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,6 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App basename={import.meta.env.BASE_URL} />
+    <App />
   </React.StrictMode>
 );
