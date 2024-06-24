364e30f53053c4a9cb25b31e12e38c1967c46d4d
Mitchell <mitchell.ramos1028@gmail.com>
2024-06-24 12:06:14 -0400

    remove models

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>", views.NoteDelete.as_view(), name="delete-note"),
     path('spotify/callback/', views.SpotifyCallbackView.as_view(), name='spotify-callback'),
 ]
